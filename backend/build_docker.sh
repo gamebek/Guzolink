c82d198c64c16027c1be5db7eb4c492a060ada94
FIBA00 <fraolbulti0@gmail.com>
2026-07-04 14:48:55 +0300

    refactor: restructure backend application and update README for Guzolink

--- backend/build_docker.sh
+++ backend/build_docker.sh
@@ -1,12 +1,12 @@
 clear
 echo "============================================================================"
-echo "Building Docker Image for Booky bookstore app"
+echo "Building Docker Image for  guzolink app"
 echo "============================================================================"
 
-docker build -t fraolbmax/booky-bookstore-app:latest .
+docker build -t fraolbmax/guzolink-app:latest .
 
 # echo "============================================================================"
 # echo "Docker Image built successfully"
 # echo "Pushing Docker Image to Docker Hub"
 # echo "============================================================================"
-# # docker push fraolbmax/booky-bookstore-app:latest
+# # docker push fraolbmax/guzolink-app:latest
