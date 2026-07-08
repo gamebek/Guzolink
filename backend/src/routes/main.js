75fde78acf5ba93d5feecbc6ba6cfd5b7009ed17
Daniel D. <brituharma@gmail.com>
2026-07-08 17:39:41 +0300

    Update main route

--- backend/src/routes/main.js
+++ backend/src/routes/main.js
@@ -1,6 +1,8 @@
 import UserRoute from "./user.route.js";
+import OrderRoute from "./orders.routes.js";
 
 export default function RegisterRoutes(app) {
 	console.log(`Registering routes: `);
 	app.use("/api/user", UserRoute);
+	app.use("/api/orders", OrderRoute);
 }
